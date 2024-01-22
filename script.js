f46d8a9ab5201fb294ba17ebbde33912fd4ac91e
h12z <141061528+h12z@users.noreply.github.com>
2024-01-22 10:57:31 +0100

    button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById('startButtonMainPage').onclick = function() {StartButton()}
-
-function StartButton() {
-    openInSameWindow("https://h12z.github.io/blindAdventure/game/game.html");
+function openInSameWindow(evt) {
+    window.location=evt;
 }
